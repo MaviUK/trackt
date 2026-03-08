b859617a80d1797e17b36547746b6879ddfb0f7e
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-03-08 12:25:10 +0000

    Create readytowatch.jsx

---
+++ src/pages/readytowatch.jsx
@@ -0,0 +1,8 @@
+export default function ReadyToWatchPage() {
+  return (
+    <div>
+      <h1>Ready to Watch</h1>
+      <p>This page will show shows with aired episodes you have not watched yet.</p>
+    </div>
+  );
+}
