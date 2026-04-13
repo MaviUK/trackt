0252ae106ceb37ffaecbc5ec004113ad25d65a36
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-04-13 18:28:04 +0100

    Update getTrendingShows.js

--- netlify/functions/getTrendingShows.js
+++ netlify/functions/getTrendingShows.js
@@ -1,3 +1,20 @@
+async function fetchExternalIds(tmdbId) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/tv/${tmdbId}/external_ids`,
+    {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_BEARER_TOKEN}`,
+      },
+    }
+  );
+
+  if (!res.ok) return null;
+
+  const json = await res.json();
+  return json;
+}
+
 export async function handler() {
   try {
     const res = await fetch(
@@ -23,15 +40,27 @@ export async function handler() {
       };
     }
 
-    const shows = (json.results || []).map((show) => ({
-      id: show.id,
-      name: show.name || "Unknown title",
-      image: show.poster_path
-        ? `https://image.tmdb.org/t/p/w500${show.poster_path}`
-        : null,
-      overview: show.overview || "",
-      year: show.first_air_date ? show.first_air_date.slice(0, 4) : null,
-    }));
+    const baseShows = json.results || [];
+
+    const shows = (
+      await Promise.all(
+        baseShows.map(async (show) => {
+          const externalIds = await fetchExternalIds(show.id);
+
+          return {
+            id: show.id,
+            tmdb_id: show.id,
+            tvdb_id: externalIds?.tvdb_id || null,
+            name: show.name || "Unknown title",
+            image: show.poster_path
+              ? `https://image.tmdb.org/t/p/w500${show.poster_path}`
+              : null,
+            overview: show.overview || "",
+            year: show.first_air_date ? show.first_air_date.slice(0, 4) : null,
+          };
+        })
+      )
+    ).filter((show) => show.tvdb_id);
 
     return {
       statusCode: 200,
