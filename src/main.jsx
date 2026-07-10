b270dbc429658959b0ae792c796d25519f283532
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-07-10 14:34:13 +0100

    Load Rankd social share helper

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import App from './App'
 import './index.css'
 import './rankd-button-fixes.css'
 import './mobile-header-consistency.css'
+import './rankd-social-share.css'
 import './rankd-scroll-fix.js'
+import './rankd-social-share.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
