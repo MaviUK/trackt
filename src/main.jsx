e7cf39ebd39fdeb637264551c5a98aed79511648
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-07-10 14:00:22 +0100

    Stop loading Rankd drag scripts

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './rankd-button-fixes.css'
-import './rankd-longpress-fix.css'
 import './mobile-header-consistency.css'
 import './rankd-scroll-fix.js'
-import './rankd-longpress-fix.js'
-import './rankd-drag-speed-fix.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
