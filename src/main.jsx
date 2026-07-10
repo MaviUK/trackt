1b07ce5dff676f31057fe4aa0fbf8a8144799425
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-07-10 12:46:27 +0100

    Load Rankd long press fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './rankd-button-fixes.css'
+import './rankd-longpress-fix.css'
 import './rankd-scroll-fix.js'
+import './rankd-longpress-fix.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
