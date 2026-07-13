242c949df6ec24834b226292122f10f4256f452b
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-07-13 19:15:29 +0100

    Implement exact paginated studio and network catalogue search

--- netlify/functions/studioCatalogueSearch.js
+++ netlify/functions/studioCatalogueSearch.js
@@ -4,10 +4,13 @@ const TVDB_BASE = "https://api4.thetvdb.com/v4";
 const TMDB_BASE = "https://api.themoviedb.org/3";
 const IMAGE_BASE = "https://image.tmdb.org/t/p";
 const MAX_TMDB_PAGE = 500;
-const CACHE_TTL_MS = 30 * 60 * 1000;
-const ENTITY_SUFFIXES = new Set([
-  "channel",
-  "company",
-  "entertainment",
-  "media",
-  "network",
+
+let tvdbToken = null;
+let tvdbTokenExpiresAt = 0;
+let genreMapCache = null;
+
+function jsonResponse(statusCode, body) {
+  return {
+    statusCode,
+    headers: {
+      "Content-Type": "application/json; charset
