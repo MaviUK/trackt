d63b85947ad21529541687822bb67cee1875eea3
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-07-14 00:19:38 +0100

    Install public show community portal at app startup

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import ProfileIssueReportMount from './components/ProfileIssueReportMount'
+import { installShowCommunityPortal } from './components/ShowCommunityPortal'
+import { supabase } from './lib/supabase'
 import { installNativeApiBridge } from './lib/installNativeApiBridge'
 import { installMobileOverscrollGuard } from './lib/installMobileOverscrollGuard'
 import { installUserCacheIsolation } from './lib/installUserCacheIsolation'
@@ -61,6 +63,7 @@ import './search-newest-first.js'
 installNativeApiBridge()
 installMobileOverscrollGuard()
 installUserCacheIsolation()
+installShowCommunityPortal(supabase)
 
 function BootReady() {
   React.useEffect(() => {
